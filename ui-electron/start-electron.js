44c5b9146cbafaac227dac24efed609f8ed12ee6
Eugene Mirotin <emirotin@gmail.com>
2017-10-30 12:44:51 +0300

    load react devtools

--- ui-electron/start-electron.js
+++ ui-electron/start-electron.js
@@ -30,7 +30,16 @@ function createWindow() {
 
   // Open the DevTools.
   if (IS_DEV) {
-    mainWindow.webContents.openDevTools();
+    const {
+      default: installExtension,
+      REACT_DEVELOPER_TOOLS
+    } = require("electron-devtools-installer");
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then(name => console.log(`Added Extension: ${name}`))
+      .catch(err => console.log("An error occurred: ", err))
+      .then(() => {
+        mainWindow.webContents.openDevTools();
+      });
   }
 
   // Emitted when the window is closed.
